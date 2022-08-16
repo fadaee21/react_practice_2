35739bc19f9614ddc46d9ef67b41dd5eaa99dd21
fadarash <fadarash@gmail.com>
2022-08-16 13:19:07 +0430

    router practice

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -2,25 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import useData from '../hooks/useData';
 
-
 export const UserList = () => {
-
   const [userDetail, loading] = useData()
-
   if (loading) {
     return <p>loading...</p>
   }
 
-
-
   return (
     <>
-
       <h3>User List</h3>
-
       <ul>
         {userDetail.map(({ id, name }) => (
           <li key={id}>
+            {/* why i must put it in template ${} */}
             <Link to={`${id}`} >
               {name}
             </Link>
